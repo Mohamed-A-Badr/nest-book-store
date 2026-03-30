c15b28a8228b3b1434eba62f09ded87ad58d245f
Mohamed-A-Badr <badr49960@gmail.com>
2026-03-30 19:25:21 +0200

    setup application with all the dependancies

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,10 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
 
+const config = new ConfigService();
 const isTest = process.env.NODE_ENV === 'test';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'sqlite',
-  database: 'db.sqlite',
+  database: isTest
+    ? config.get<string>('TEST_DB_NAME', 'test.sqlite')
+    : config.get<string>('DB_NAME', 'db.sqlite'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: isTest ? [] : [__dirname + '/../../migrations/*{.ts,.js}'],
   synchronize: isTest,
